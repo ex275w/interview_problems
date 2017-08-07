638420e8f29283cb3b9bd61505b796ed181f18ad
ex275w <ex275w@gmail.com>
2017-08-07 13:51:29 -0500

    Second Commit

--- HighestChar.java
+++ HighestChar.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class HighestChar {
@@ -11,41 +10,35 @@ public class HighestChar {
 		System.out.println(getMostRepeatedCharacter("YXyXyXyXY")); //X
 		System.out.println(getMostRepeatedCharacter(null)); //blank space or another suggestion
 		System.out.println(getMostRepeatedCharacter("")); //blank space or another suggestion   
-		System.out.println(getMostRepeatedCharacter("ivan.el.guapo.123@gmail.com"));
+		System.out.println(getMostRepeatedCharacter("ivan.el.guapo.123@gmail.com")); //.
 	}
-	
+
 	public static char getMostRepeatedCharacter(String data){
 		if (data == null)
 			return ' ';
-		
+
 		if (data == "")
 			return ' ';
-		
+
 		char [] charArray = data.toCharArray();
-		Arrays.sort(charArray);
 		HashMap<Character, Integer> hash = new HashMap<>();
-		int counter = 0;
-		char currchar = charArray[0];
-		
+
 		for(int i = 0; i < charArray.length; i++){
-			if (currchar != charArray[i]){
-				hash.put(currchar, counter);
-				currchar = charArray[i];
-				counter = 1;
-			}
+			if(hash.get(charArray[i]) == null)
+				hash.put(charArray[i], 1);
 			else
-				counter++;
+				hash.put(charArray[i], hash.get(charArray[i]) + 1);
 		}
-		
-		char returnchar = 0;
+
+		char returnchar = ' ';
 		int biggestnumber = 0;
-		
+
 		for(char key: hash.keySet()){
 			if(biggestnumber < hash.get(key)){
 				biggestnumber = hash.get(key);
 				returnchar = key;
 			}
 		}
-	    return returnchar;
+		return returnchar;
 	}
 }
